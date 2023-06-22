af884172f430d220965fe71fff5849f478c1da3c
RyFax <ryfax@git>
2023-06-23 00:12:14 +0200

    Fix

---
+++ Sources/Visualization/Microsoft.Psi.Visualization.Windows/Views/Visuals2D/PlayersDataVisualizationObjectView.xaml.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+#pragma warning disable
+
+namespace Microsoft.Psi.Visualization.Views.Visuals2D
+{
+    using Microsoft.Psi.Visualization.VisualizationObjects;
+    using System.Collections.Generic;
+    using System.Windows.Documents;
+
+    /// <summary>
+    /// Interaction logic for PlayersDataVisualizationObjectView.xaml.
+    /// </summary>
+    public partial class PlayersDataVisualizationObjectView : VisualizationObjectView
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PlayersDataVisualizationObjectView"/> class.
+        /// </summary>
+        public PlayersDataVisualizationObjectView()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
